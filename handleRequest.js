84c72a4aea67b1233df96716019ea8648ffe9862
Maxwell-Alexius <destinydragon85614@gmail.com>
2017-11-29 15:27:48 +0800

    Able to load FileSystem's and Notebook's state from JSON file via backend HTTP server

--- handleRequest.js
+++ handleRequest.js
@@ -9,11 +9,35 @@ function jsonResponse(response, file) {
   })
 }
 
+
+function processPostRequest(request, callback) {
+  if (request.method == 'POST') {
+    let body = '';
+
+    request.on('data', function (data) {
+      body += data;
+      // Too much POST data, kill the connection!
+      // 1e6 === 1 * Math.pow(10, 6) === 1 * 1000000 ~~~ 1MB
+      if (body.length > 1e6) request.connection.destroy()
+    });
+
+    request.on('end', function () {
+      callback(JSON.parse(body))
+    });
+  }
+}
+
 function handleRequest(request, response) {
   switch(request.url) {
     case '/load_file_system':
       jsonResponse(response, './data/fileSystem.json')
       break
+
+    case '/load_notebook':
+      processPostRequest(request, ({ id }) => {
+        jsonResponse(response, `./data/notebooks/_${id}.json`)
+      })
+      break;
 
     default:
       response.writeHead(404, { "Content-Type": "text/plain" })
